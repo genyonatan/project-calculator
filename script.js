6b28132e5b6f35967dbe0f70119491a6072f2da3
Yonatan Genene <yonatangenene30@gmail.com>
2025-11-24 14:18:27 +0300

    - Turn on and off by clicking on display. - on and off styles.

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-let calcState = {
+let calcStates = {
   offState: "OFF",
   initialInputState: "FIRST INPUT",
   operandInputState: "OPERATOR INPUT",
   secondInputState: "SECOND INPUT",
 };
 
+let calcState = calcStates.offState;
 isDecimal = true;
 initialPrompt = "Clcik on Display to start";
 
 const promptEl = document.querySelector(".promptDiv");
 promptEl.textContent = initialPrompt;
-const displayEl = document.querySelector(".calculatory-display");
+const displayEl = document.querySelector(".calculator-display");
 const calBtns = document.querySelector(".calc-btns");
 
 function add(a, b) {
@@ -45,5 +46,19 @@ function operate(operator, a, b) {
   }
 }
 
-const calcGrid = document.querySelector(".calc-btns");
-const numericBtns = document.createElement(div);
+displayEl.addEventListener("click", () => {
+  switch (calcState) {
+    case calcStates.offState: // to turn the calculator On.
+      displayEl.classList.add("displayOn");
+      promptEl.textContent = "enter a number";
+      calcState = calcStates.initialInputState;
+      break;
+    default: //to turn the calculator Off.
+      calcState = calcStates.offState;
+      promptEl.textContent = initialPrompt;
+      displayEl.classList.toggle("displayOn");
+      break;
+  }
+});
+
+calBtns.addEventListener("click", () => {});
